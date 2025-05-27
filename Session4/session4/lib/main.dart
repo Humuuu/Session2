b81d4d6b02dec8dea1d34ca054266e58485a6f2e
Humuuu <graciana.allegra@binus.ac.id>
2025-05-27 23:43:44 +0700

    Sesi6

--- Session4/session4/lib/main.dart
+++ Session4/session4/lib/main.dart
@@ -1,20 +1,32 @@
 import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
+
 import 'package:provider/provider.dart';
 import '../providers/cart_provider.dart';
 import '../providers/product_provider.dart';
 import '../screens/cart_screen.dart';
 import '../screens/product_screen_list.dart';
+import '../screens/auth_screen.dart'; 
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+
   runApp(
-    MultiProvider(providers: [
-      ChangeNotifierProvider(
-        create: (context) => ProductProvider(),
-      ),
-      ChangeNotifierProvider(
-        create: (context) => CartProvider(),
-      ),
-    ], child: MyApp(),)
+    MultiProvider(
+      providers: [
+        ChangeNotifierProvider(
+          create: (context) => ProductProvider(),
+        ),
+        ChangeNotifierProvider(
+          create: (context) => CartProvider(),
+        ),
+      ],
+      child: const MyApp(),
+    ),
   );
 }
 
@@ -28,8 +40,9 @@ class MyApp extends StatelessWidget {
       routes: {
         '/': (context) => const ProductListScreen(),
         '/cart': (context) => const CartScreen(),
+        '/auth': (context) => const AuthScreen(), 
       },
-      initialRoute: '/',
+      initialRoute: '/auth',
     );
   }
 }
